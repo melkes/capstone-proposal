3e93b45b834a8bbdd89c6c61e5126a1156a802d3
Mike Wilkes <mikewilkes@outlook.com>
2023-07-16 05:00:42 -0700

    Add language selector to form

--- podcast-translation-app/src/components/PodcastForm.js
+++ podcast-translation-app/src/components/PodcastForm.js
@@ -3,19 +3,24 @@ import React, { useState } from "react";
 function PodcastForm() {
 
   const [podcastData, setPodcastData] = useState(""); // initialize state
-  
+  const [language, setLanguage] = useState("en"); // initialize state, default to english
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(podcastData); // log state
+    console.log(podcastData, language); // log state
   }
 
   const handleChange = (event) => {
-    setPodcastData(event.target.value); // update state
+    setPodcastData(event.target.files[0]); // update state with file
+  }
+
+  const handleLanguageChange = (event) => {
+    setLanguage(event.target.value); // update state with language choice
   }
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="podcast-form"> {/* To do: add styles for this */}
         <input 
           type="file"
           name="podcastData"
@@ -23,6 +28,25 @@ function PodcastForm() {
           value={podcastData} // set value
           onChange={handleChange} // update state when changed
           />
+        <select 
+          name="language"
+          value={language} // set value
+          onChange={handleLanguageChange} // update state when changed
+        >
+          <option value="en">English</option>
+          <option value="es">Spanish</option>
+          <option value="fr">French</option>
+          <option value="de">German</option>
+          <option value="it">Italian</option>
+          <option value="pt">Portuguese</option>
+          <option value="zh">Chinese</option>
+          <option value="ja">Japanese</option>
+          <option value="ko">Korean</option>
+          <option value="ar">Arabic</option>
+          <option value="ru">Russian</option>
+          <option value="hi">Hindi</option>
+        </select>
+        <input type="submit" value="Submit" />
       </form>
     </div>
   );
