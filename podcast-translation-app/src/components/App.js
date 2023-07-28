dd37a3b17bf06714eaef1e25d6f6624e9d4f028f
Mike Wilkes <mikewilkes@outlook.com>
2023-07-28 16:06:37 -0700

    WIP getting app back to working state

--- podcast-translation-app/src/components/App.js
+++ podcast-translation-app/src/components/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from './Header'; // Import the Header component
-import AppController from './AppController';
+import PodcastForm from './PodcastForm';
 
 function App() {
   return (
     <div className="container">
       <Header />
       <div style={{ margin: '20px 0' }} /> 
-      <AppController />
+      <PodcastForm />
     </div>
   );
 }
