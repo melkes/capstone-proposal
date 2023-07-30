a6063d1b2bf550eedbb1350795214ef60895a9a3
Mike Wilkes <mikewilkes@outlook.com>
2023-07-30 06:15:47 -0700

    Added links, updated styling

--- src/components/PodcastForm.js
+++ src/components/PodcastForm.js
@@ -8,7 +8,7 @@ function PodcastForm(props) {
     display: 'inline-block',
     width: '20rem',
     height: '3.375rem',
-    backgroundColor: '#7777FF',
+    backgroundColor: 'mediumseagreen',
     color: '#FFF',
     padding: '0 0.5rem',
     fontSize: '1em',
@@ -73,16 +73,16 @@ function PodcastForm(props) {
   
   return (
     <div>
-      <h1>Welcome to our Podcast Translation service!</h1>
-      <p>To get started, upload a podcast file from your computer or <s>enter the URL</s>.</p>
-      <p className="main-text">
+      <h2>Create English-language transcripts from foreign language podcasts</h2>
+
+      <p className="main-text">To get started, upload a podcast file from your computer or <s>enter the URL</s>.</p>
+      <p>
       Need a file to try out? Try right-clicking (or long-press if you're on a mobile device) on <a href="https://traffic.omny.fm/d/clips/60311b15-274a-4e3f-8ba9-ac3000834f37/5fd6f200-265f-42c7-ab4e-ae9a00ce4223/ee83a506-8eb1-47f8-8b4c-ae9a00ce62d2/audio.mp3" download>this link</a> to download a trailer for an Italian food podcast, or on <a href="https://www.nhk.or.jp/s-media/news/podcast/audio/7c2248ff0f5131f1ce60a6f6451a1651_64k.mp3" download>this link</a> to get a Japanese news update. After downloading, you can then upload the file via the form below.
       </p>
       <p className="main-text">Not sure where to find foreign podcasts? Here are a few places to start:</p>
       <ul>
-        <li><a href="#">Foreign Language Podcast Website 1</a></li>
-        <li><a href="#">Foreign Language Podcast Website 2</a></li>
-        <li><a href="#">Foreign Language Podcast Website 3</a></li>
+      <li><a href="https://www.podcasts-online.org/discover/">Podcasts-Online.org</a>. Scroll to "Listen to podcasts from all over the world" at the bottom of the page.</li>
+      <li><a href="https://chartable.com/charts">Chartable</a> has global and country specific top podcast charts</li>
       </ul>
 <hr/>
       <form onSubmit={handleSubmit} className="podcast-form"> {/* To do: add styles for this */}
@@ -90,10 +90,11 @@ function PodcastForm(props) {
         <input 
           type="text"
           name="podcastUrl"
-          placeholder="Enter a URL"
+          placeholder="Enter a URL (coming soon)"
           onChange={(event) => setPodcastUrl(event.target.value)} // update state when URL is entered
+          disabled // disable input until feature available
         />
-        <p>Or</p>
+        {/* <p>Or</p> */}
         <input 
           type="file"
           name="podcastData"
@@ -101,7 +102,7 @@ function PodcastForm(props) {
           onChange={handleChange} // update state when changed
           />
           <div>
-          <p class="secondary-text">Currently you'll need to download the file to your computer and then upload it via this form. Support for URLs is coming!</p>
+          <p className="secondary-text">Currently you'll need to download the file to your computer and then upload it via this form. Support for URLs is coming!</p>
           </div>
         <input type="submit" style={buttonStyle} value="Get transcript" />
       </form>
